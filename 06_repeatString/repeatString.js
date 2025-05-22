edf74869ee6ea27bb5e23dd531fd61b70cf3aedf
allan-anilson <allananilson@gmail.com>
2025-05-22 21:43:34 +0530

    finished exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    res = "";
+    for(let i = 0; i<n; i++){
+        res+=str;
+    }
+    return res;
 };
 
 // Do not edit below this line

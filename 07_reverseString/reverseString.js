d0e825fad85dc1dd4fe87a4cb48c092f0aa0a38c
allan-anilson <allananilson@gmail.com>
2025-05-23 12:43:18 +0530

    completed until exercise 11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let len = str.length;
+    let arr = str.split("");
+    for(let i = 0; i<len/2; i++){
+        let temp = arr[i];
+        arr[i] = arr[len-i-1];
+        arr[len-i-1] = temp;
+        
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fdeg) {
+  let cdeg = (fdeg-32) *(5/9);
+  cdeg = Math.round(cdeg*10)/10;
+  return cdeg;
+  
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cdeg) {
+  let fdeg = (cdeg*(9/5))+32;
+  fdeg = Math.round(fdeg*10)/10;
+  return fdeg;
+  
+  
 };
 
 // Do not edit below this line
